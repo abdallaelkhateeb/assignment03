50dc3d8ed755833b438dd94662fef105c800fee7
abdallaelkhateeb <119896180+abdallaelkhateeb@users.noreply.github.com>
2025-06-29 07:27:14 +0300

    q1

--- assignment03/Program.cs
+++ assignment03/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace assignment03
+﻿using System.ComponentModel;
+
+namespace assignment03
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region q1
+            //1 - Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no.
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine((num % 3 == 0 && num % 4 == 0) ? "Yes" : "No");
+
+            #endregion
+
         }
     }
 }
