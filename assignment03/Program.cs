f8d852394b186a1152aeef31e990cca553092b64
abdallaelkhateeb <119896180+abdallaelkhateeb@users.noreply.github.com>
2025-06-29 07:24:27 +0300

    Add project files.

---
+++ assignment03/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace assignment03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
