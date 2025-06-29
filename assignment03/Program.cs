fecdfc9ca71bad23639f757d94bb4274d4a5f808
abdallaelkhateeb <119896180+abdallaelkhateeb@users.noreply.github.com>
2025-06-29 07:30:08 +0300

    q2,3

--- assignment03/Program.cs
+++ assignment03/Program.cs
@@ -13,6 +13,31 @@ namespace assignment03
 
             #endregion
 
+            #region q2
+           // 2 - Write a program that allows the user to insert an integer then print negative if it is negative number otherwise print positive.
+
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(number < 0 ? "Negative" : "Positive");
+            #endregion
+
+
+            #region q3
+            // 3- Write a program that takes 3 integers from the user then prints the max element and the min element.
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            int max = Math.Max(a, Math.Max(b, c));
+            int min = Math.Min(a, Math.Min(b, c));
+
+            Console.WriteLine("max element = " + max);
+            Console.WriteLine("min element = " + min);
+
+
+            #endregion
+            #region q4
+            
+            #endregion
         }
     }
 }
